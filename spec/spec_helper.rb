b726c8520571a687f4ded65c64ae5e019c2dd488
Katsuhiko YOSHIDA <claddvd@gmail.com>
2022-05-28 09:59:39 +0900

    Make temp directory stub

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,7 @@ RSpec.configure do |config|
     path = File.join(TMP_DIR, "schema.json")
     Dir.mkdir(TMP_DIR) unless Dir.exist?(TMP_DIR)
     FileUtils.remove_file(path) if File.exist?(path)
+    stub_const("Bundler::Alive::USER_PATH", TMP_DIR)
     stub_const("Bundler::Alive::SCHEMA_PATH", path)
 
     allow(ENV).to receive(:fetch).and_call_original
