2f8a7bcb5fefbdd5aa50aecec3e583c92f17d9e6
Katsuhiko YOSHIDA <claddvd@gmail.com>
2022-05-28 10:54:45 +0900

    Supress re-load constants warning

--- spec/bunlder/alive/client/github_graphql_spec.rb
+++ spec/bunlder/alive/client/github_graphql_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe "Bundler::Alive::Client::GithubGraphql" do
   context "when a schema file does not exist" do
     it "dump schema file" do
       VCR.use_cassette "github.com/schema.json" do
+        if Bundler::Alive::Client.const_defined?(:GithubGraphql)
+          Bundler::Alive::Client.send(:remove_const, :GithubGraphql)
+        end
+
         load File.expand_path("../../../../lib/bundler/alive/client/github_graphql.rb", File.dirname(__FILE__))
         obj = Object.new
         obj.extend Bundler::Alive::Client::GithubGraphql
